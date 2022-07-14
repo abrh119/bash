7f1b8df575d1aa267f792644c180d9b4afd8337e
Abdur Rahman <59731843+abrh119@users.noreply.github.com>
2022-07-14 08:27:41 +0530

    NOT EXACTLY COLUMN ROW BASED PYRAMID

--- pyramidOfStars.bash
+++ pyramidOfStars.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# CODE 1
+
 makePyramid()
 {
   #Here $1 is the parameter you passed with the function i,e 5
@@ -35,3 +38,28 @@ makePyramid()
 
 #change number according to your need
 makePyramid 5
+
+#CODE 2
+#!/bin/bash
+
+clear
+echo -n "Enter the level of pyramid: "; read n
+
+star=''
+space=''
+
+for ((i=0; i<n; i++ ))
+do
+space="$space "
+done
+
+
+
+for (( i=1; i<n; i++ ))
+do
+
+star="$star*"
+space="${space%?}"
+echo "$star";
+
+done
