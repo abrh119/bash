7b2d6de9264a26f69ebad50f532a1f63720a836e
Abdur Rahman <59731843+abrh119@users.noreply.github.com>
2022-07-14 11:28:27 +0530

    half pyramid

--- pyramidOfStars.bash
+++ pyramidOfStars.bash
@@ -1,1 +1,26 @@
 Correct: https://www.tutorialsandyou.com/bash-shell-scripting/pyramid-and-pattern-10.html
+
+#CODE 2
+#!/bin/bash
+
+clear
+echo -n "Enter the level of pyramid: "; read n
+
+star=''
+space=''
+
+for ((i=0; i<n; i++ ))
+do
+space="$space "
+done
+
+
+
+for (( i=1; i<n; i++ ))
+do
+
+star="$star*"
+space="${space%?}"
+echo "$star";
+
+done
